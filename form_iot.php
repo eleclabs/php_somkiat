bb5d5c7b815a719c1eb0534e4f718fd0de800809
eleclabs <eleclabs@gmail.com>
2025-07-01 10:59:41 +0700

    update

--- form_iot.php
+++ form_iot.php
@@ -1,6 +1,6 @@
 <form action="insert_iot.php" method="post">
-    Zone Name: <input type="text" name="zone" /> <br/>
-    Temp: <input type="text" name="temp" /> <br/>
+    Zone: <input type="text" name="zone" /> <br/>
+    Tempolary: <input type="text" name="temp" /> <br/>
     Humidity: <input type="text" name="hum" /> <br/>
     User: <input type="text" name="user" /> <br/>
     <input type="submit" value="บันทึกข้อมูล" />
