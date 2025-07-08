6ab8a2a9f80e4cc3f950f6ede77c4a9261a63ba9
eleclabs <eleclabs@gmail.com>
2025-07-08 11:24:29 +0700

    update

--- header.php
+++ header.php
@@ -2,8 +2,16 @@
     <li><a href="index.php">Home</a></li>
     <li><a href="about.php">About</a></li>
     <li><a href="contact.php">Contact</a></li>
-    <li><a href="login.php">Login</a></li>
-    <li><a href="register.php">Register</a></li>  
-    <li><a href="profile.php">Profile</a></li>     
- 
+
+    <?php
+        if (isset($_SESSION['Email'])) {
+            echo '<li><a href="profile.php">Profile</a></li>';  
+            echo '<li><a href="logout.php">Logout</a></li>';
+        } else {
+            echo '<li><a href="login.php">Login</a></li>';
+            echo '<li><a href="register.php">Register</a></li>';
+        }
+    ?> 
+
+    
 </ul>
